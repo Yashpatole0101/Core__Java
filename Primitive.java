b6b7fe4c81708a27a62f9c3d6a86b9c9f8e8d324
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-08 21:05:06 +0530

    Primitive2 datatype

---
+++ Primitive.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		boolean f1=true;
+		boolean f2=false;
+		System.out.println("java is easy is lang."+f1);
+		
+		
+		
+		int a=30;
+		System.out.println("todays temperature is="+a+"oc");
+		
+		
+		
+	
+
+	}
+
+}
