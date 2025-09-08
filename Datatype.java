583c2b7f6bc468632b8c93acd08e07a87b7c96ca
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-08 20:54:36 +0530

    primitive datatypes

---
+++ Datatype.java
@@ -0,0 +1,39 @@
+package day2;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		int age=21;    //store whole number
+		System.out.println(age);  
+		
+		
+		
+		float salary=26.35f;
+		System.out.println(salary);
+		
+		
+		
+		double bankbalance=4568.8222;
+		System.out.println(bankbalance);
+		
+		
+		
+		long mo_no=123456789;
+		System.out.println(mo_no);
+		
+		
+		char gender='M';
+		System.out.println(gender);
+		
+		
+		String str="hello good morning!!";
+		System.out.println(str);
+		
+		
+		System.out.println(salary+age+bankbalance+" "+ str+100);
+		System.out.println(gender+age+str);
+		
+		String tr="10";
+		System.out.println(age+tr);
+		
+	}}
