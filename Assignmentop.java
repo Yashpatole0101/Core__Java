25e062e7e145bc2572fb4e5f56ba0734eca0307a
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-08 21:18:43 +0530

    Assignment(=,+=,-=,*=,/=,%=)

---
+++ Assignmentop.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Assignmentop {
+
+	public static void main(String[] args) {
+		//assignmentop- (=,+=,-=,*=,/=)
+		
+		int a=10;
+		
+		a+=3;
+		System.out.println(a);
+		
+     	a-=4;
+		System.out.println(a);
+		
+		
+		a*=4;
+    	System.out.println(a);
+    	
+    	a/=2;
+    	System.out.println(a);
+    	
+    	
+    	a%=4;
+    	System.out.println(a);
+    	
+    	
+	}}
