43fc1120a65246916554ad645485f0b1a7922e4f
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-08 21:20:58 +0530

    Relationalop(==,!=,<,>,<=,>=)

---
+++ Relationalop.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Relationalop {
+
+	public static void main(String[] args) {
+		// relational operators,returns value in true or false
+		
+		int a=10;
+		int b=10;
+		System.out.println(a==b);
+		
+		int c=12;
+		System.out.println(a==c);
+		
+		
+		System.out.println(a<=b);
+		
+		System.out.println(b!=c);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
