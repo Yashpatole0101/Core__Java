c0d3c01dd44d75bdb8c8bdc33c956ccf66c010b3
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-08 21:11:56 +0530

    Casting(implicit&explicit)

---
+++ Casting.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		//explicit casting
+		double a=234.88;
+		int d=(int)a;
+		System.out.println(a);
+		System.out.println(d);
+		
+		
+		//implicit casting
+		int i=34;
+		double q=i;
+		System.out.println(i);
+		System.out.println(q);
+		
+
+	}
+
+}
