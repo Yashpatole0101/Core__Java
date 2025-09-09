e215588325fcc19ac892787ebbce396bc0e64e9e
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-09 12:03:57 +0530

    Logical operator(AND/&&)

---
+++ Logicalop.java
@@ -0,0 +1,38 @@
+package day3;
+
+public class Logicalop {
+
+	public static void main(String[] args) {
+//		System.out.println(true&&false);
+//		System.out.println(false&&true);
+//		System.out.println(false&&false);
+//		System.out.println(true&&true);
+
+		
+		int age=20;
+		int age1=34;
+		
+		System.out.println(age>30&&age1>60);
+		System.out.println(age<30&&age1<35);
+		
+		
+		
+		
+		
+		System.out.println(24<100&&24>10);
+		
+		
+		//checking the person who statisfying both the conditions to take benefits for bank
+		
+		double bank_balance=520000.5688;
+		System.out.println(bank_balance>300000&&bank_balance>500000&&bank_balance<600000);
+		
+		
+		
+		double bankbalance=200000.68;
+		System.out.println(bankbalance>300000&&bankbalance>500000&&bankbalance<600000);
+		
+
+	}
+
+}
