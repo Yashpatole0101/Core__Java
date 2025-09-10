7ec5188b2c595d1f59aec5f9d322ec0f8906408b
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-10 11:54:56 +0530

    Logical operator(0R,NOT)

---
+++ Logical_or_not.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class Logical_or_not {
+
+	public static void main(String[] args) {
+		//OR(||) operator
+		//t||f=>t
+		//f||t=>t
+		//t||t=>t
+		//f||f=>f
+		
+		
+		int a=19;
+		int b=67;
+		
+		System.out.println(a>=b||b==a);
+		System.out.println((23==56)||(23>4));
+		
+		//logical NOT(!)
+		//!true=>galse
+		//!false=>true
+		
+		
+		System.out.println(!(a<b));
+	
+				
+
+	}
+
+}
