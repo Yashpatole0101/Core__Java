a937546334aadde1d43fc83d83676bd421eaa895
Yashpatole0101 <yash0atole420@gmail.com>
2025-09-22 11:43:50 +0530

    do while loop

---
+++ Do_while.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class Do_while {
+
+	public static void main(String[] args) {
+		int num=1;
+		do {
+			System.out.println("hii");
+			num++;
+		}
+		while(num<=5);
+		
+		//table of 9
+		int t=9;
+		int i=1;
+		do
+		{
+			System.out.println(t*i);
+			i++;
+		}
+		while(i<=10);
+
+	}
+
+}
